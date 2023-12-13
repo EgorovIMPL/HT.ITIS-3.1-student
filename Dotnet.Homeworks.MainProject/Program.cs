9fa255643879a56ccb57c2ab0d1c2a07dc700186
User impl_zdraste <egorov.r.684017@gmail.com>
2023-12-13 16:41:33 +0300

    Homework 5

--- Dotnet.Homeworks.MainProject/Program.cs
+++ Dotnet.Homeworks.MainProject/Program.cs
@@ -1,9 +1,20 @@
 using Dotnet.Homeworks.Data.DatabaseContext;
+using Dotnet.Homeworks.MainProject.Configuration;
 using Dotnet.Homeworks.MainProject.Services;
+using Dotnet.Homeworks.MainProject.ServicesExtensions.Masstransit;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var rabbitmq = new RabbitMqConfig
+{
+    Username = builder.Configuration["RabbitMqConfig:Username"]!,
+    Password = builder.Configuration["RabbitMqConfig:Password"]!,
+    Hostname = builder.Configuration["RabbitMqConfig:Hostname"]!,
+    Port = int.Parse(builder.Configuration["RabbitMqConfig:Port"]!)
+};
+
 builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -17,6 +28,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMasstransitRabbitMq(rabbitmq!);
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
